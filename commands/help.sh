d1f4e6ed096978fb48c1a16a229e3fd89117e633
Milan van As <milanvanas@gmail.com>
2023-02-28 13:59:24 +0100

    Updated help

--- commands/help.sh
+++ commands/help.sh
@@ -13,6 +13,7 @@ help() {
   echo "Usage:"
   echo "infrabackup show                                  - Shows all configs"
   echo "infrabackup backup <backupName>                   - Run the backup, replace <backupName> with the directory name as defined in ./configs"
+  echo "infrabackup purge <backupName>                    - Run the purge, replace <backupName> with the directory name as defined in ./configs"
   echo "infrabackup cron <enable|disable> <backupName>    - Enable or disable the backup cron (uses /etc/cron.d/ symlinks)"
   echo "infrabackup create <backupName>                   - Initialize a new backup config directory in <infrabackup>/configs"
   echo "infrabackup validate-system                       - Validates the system, checks all required commands (rsync/rdiff-backup/etc)"
